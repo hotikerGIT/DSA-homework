b8745ce1121a33b449458f5ba4f11a7f9bd093d6
hotikerGIT <m.m.grevtsov@gmail.com>
2026-04-23 16:07:23 +0300

    bits e g

--- training/ded/bits/E.cpp
+++ training/ded/bits/E.cpp
@@ -1,3 +1,30 @@
-//
-// Created by Misha on 23.04.2026.
-//
+#include <iostream>
+#include <string>
+#define int long long
+
+int char_to_b64(char c) {
+    if ('0' <= c && c <= '9') return c - '0';
+    if ('A' <= c && c <= 'Z') return c - 'A' + 10;
+    if ('a' <= c && c <= 'z') return c - 'a' + 36;
+    if (c == '-') return 62;
+    if (c == '_') return 63;
+    return -1;
+}
+
+signed main() {
+    std::string s;
+    std::cin >> s;
+
+    int ans = 1;
+    for (char c : s) {
+        int num = char_to_b64(c);
+
+        for (int j = 0; j < 6; ++j) {
+            int bit = (num >> j) & 1;
+            ans *= bit ? 1 : 3;
+            ans %= (int)1e9 + 7;
+        }
+    }
+
+    std::cout << ans;
+}
--- training/ded/bits/G.cpp
+++ training/ded/bits/G.cpp
@@ -1,3 +1,24 @@
-//
-// Created by Misha on 23.04.2026.
-//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define int long long
+
+signed main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> basis;
+    for (int i = 0; i < n; ++i) {
+        int a;
+        std::cin >> a;
+        for (int b : basis) {
+            a = std::min(a, a ^ b);
+        }
+        if (a > 0) basis.push_back(a);
+    }
+    std::sort(basis.begin(), basis.end(), std::greater<>());
+    int result = 0;
+    for (int b : basis) {
+        result = std::max(result, result ^ b);
+    }
+    std::cout << result;
+}
