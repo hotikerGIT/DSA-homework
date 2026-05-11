01c5f06cef5acf74a1b81f4fd3c07f6de8d99d40
Mihail <mihail@MacBook-Air-Mihail-4.local>
2026-05-11 10:28:09 +0300

    atcoder

--- olympiad-contests/atcoder/2026/04-april/25/B.cpp
+++ olympiad-contests/atcoder/2026/04-april/25/B.cpp
@@ -1,3 +1,34 @@
-//
-// Created by Mihail on 25.04.2026.
-//
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<std::string> strings(n);
+    for (int i = 0; i < n; ++i) std::cin >> strings[i];
+
+    int ans = 0;
+    for (int h1 = 0; h1 < n; ++h1) {
+        for (int h2 = h1; h2 < n; ++h2) {
+            for (int w1 = 0; w1 < m; ++w1) {
+                for (int w2 = w1; w2 < m; ++ w2) {
+                    bool flag = true;
+
+                    for (int i = h1; i <= h2; ++i) {
+                        for (int j = w1; j <= w2; ++j) {
+                            if (strings[h1 + h2 - i][w1 + w2 - j] != strings[i][j]) {
+                                flag = false;
+                                break;
+                            }
+                        }
+                    }
+
+                    ans += flag;
+                }
+            }
+        }
+    }
+
+    std::cout << ans;
+}
--- olympiad-contests/atcoder/2026/04-april/25/C.cpp
+++ olympiad-contests/atcoder/2026/04-april/25/C.cpp
@@ -1,3 +1,32 @@
-//
-// Created by Mihail on 25.04.2026.
-//
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#define int long long
+
+signed main() {
+    int n, k;
+    std::cin >> n >> k;
+    std::map<int, int> greedy;
+    std::vector<int> a(n);
+    int ans = 0;
+
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i];
+        greedy[a[i]] += a[i];
+        ans += a[i];
+    }
+
+    std::vector<int> best_sum;
+    for (auto& [val, sum] : greedy) {
+        best_sum.push_back(sum);
+    }
+    std::sort(best_sum.rbegin(), best_sum.rend());
+
+    for (int i = 0; i < k && i < (int)best_sum.size(); ++i) {
+        ans -= best_sum[i];
+    }
+
+    std::cout << ans;
+}
